ad3c60fffbef644365075ac6f45759ea94acd37b
George Muscat <georgemuscat@proton.me>
2023-08-01 15:16:36 +1000

    test

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express, { Express, Request, Response, json } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response, json } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,15 +7,24 @@ const app: Express = express();
 app.use(json());
 const port = process.env.PORT;
 const host = process.env.HOST;
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+const webhook = process.env.WEBHOOK as string;
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
-app.post('/hook', (req: Request, res: Response) => {
-    console.log(req.body); // Call your action on the request here
-    res.status(200).end();
+app.post("/hook", (req: Request, res: Response) => {
+  if (req.body.action === "opened" && req.body.pull_request !== undefined)
+    // Forward to webhook
+    fetch(webhook, {
+      method: "POST",
+      body: JSON.stringify(req.body),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  res.status(200).end();
 });
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://${host}:${port}`);
+  console.log(`⚡️[server]: Server is running at http://${host}:${port}`);
 });
